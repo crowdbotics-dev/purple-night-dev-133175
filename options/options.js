4298d0fb483283a2a1bf89f7deda6b3d8298639a
Crowdbotics <engineering@crowdbotics.com>
2024-02-09 18:29:14 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "purple-night-dev-133175",
+  url: "https://purple-night-dev-133175.botics.co",
+  api: "https://purple-night-dev-133175.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
